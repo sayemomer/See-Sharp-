89d3408416d8486e7caad234c7fd1a50c7e80672
osOmer <omer_sayem@yahoo.com>
2018-02-04 12:41:11 +0600

    Add files via upload

---
+++ settterGetter/Mainclass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    class Mainclass
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.set_name("durjoy");
+            s.set_id("16");
+            s.set_cgpa(4.00);
+            Console.WriteLine(s.get_name());
+            Console.WriteLine(s.get_id());
+            Console.WriteLine(s.get_cgpa());
+        }
+    }
+}
---
+++ settterGetter/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    class Student
+    {
+        private string name;
+        private string id;
+        private double cgpa;
+        public void set_name(string n)
+        {
+            name = n;
+        }
+        public void set_id(string i)
+        {
+            id = i;
+        }
+        public void set_cgpa(double cg)
+        {
+            cgpa = cg                                                                                                                                                                                                                                                                     ;
+        }
+        public string get_name()
+        {
+            return name;
+        }
+        public string get_id()
+        {
+            return id;
+        }
+        public double get_cgpa()
+        {
+            return cgpa;
+        }
+
+
+    }
+}
