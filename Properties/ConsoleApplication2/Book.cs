1d07465ef5f93e00f70d6591da4c3e5f97fa008f
osOmer <omer_sayem@yahoo.com>
2018-02-04 12:42:49 +0600

    Add files via upload

---
+++ Properties/ConsoleApplication2/Book.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Book
+    {
+
+        private int bookID;
+        private string bookTitle;
+        private string authorName;
+        private int page;
+        private int publicationYear;
+        private double balance;
+
+
+
+        public int BookID
+        {
+
+            set { this.bookID = value; }
+            get { return bookID; }
+
+
+        }
+
+
+        public string BookTitle
+        {
+
+            set { this.bookTitle = value; }
+            get { return bookTitle; }
+        }
+
+
+        public string AuthorName
+        {
+
+            set { this.authorName = value; }
+            get { return authorName; }
+        }
+
+
+
+        public int Page
+        {
+            set { this.page = value; }
+            get { return page; }
+
+        }
+
+
+
+        public int PublicationYear
+        {
+            set { this.publicationYear = value; }
+            get { return publicationYear; }
+
+        }
+
+
+
+        public double Balane
+        {
+            set { this.balance = value; }
+            get { return balance; }
+
+
+        }
+    }
+}
---
+++ Properties/ConsoleApplication2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Book newBook = new Book();
+
+
+
+
+            newBook.BookID = 123;
+            newBook.BookTitle=" C#";
+            newBook.AuthorName=" Doglouys";
+            newBook.Page=39;
+            newBook.PublicationYear=2020;
+            newBook.Balane=100.20;
+
+
+            Console.WriteLine( newBook.BookID );
+            Console.WriteLine( newBook.BookTitle );
+            Console.WriteLine( newBook.AuthorName );
+            Console.WriteLine( newBook.Page );
+            Console.WriteLine( newBook.PublicationYear );
+            Console.WriteLine( newBook.Balane  );
+
+
+        }
+    }
+}
