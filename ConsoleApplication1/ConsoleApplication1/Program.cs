e5b9f6044816309775a1643bd00990aa8fe124b3
osOmer <omer_sayem@yahoo.com>
2018-02-04 12:43:58 +0600

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Account obj = new Account();
+
+            obj.setAccountID(123);
+            obj.setAccountHolderName("omer sayem");
+            obj.setBalance(500);
+
+
+            Console.WriteLine( obj.getAccountID() );
+            Console.WriteLine( obj.getAccounHolderName() );
+            Console.WriteLine( obj.getBalance() );
+
+
+        }
+    }
+}
---
+++ ConsoleApplication1/SetterGetter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Account
+    {
+        private int accountID;
+        private string accountHolderName;
+        private double balance;
+
+
+        public void setAccountID(int accountID)
+        {
+            this.accountID = accountID;
+        }
+
+        public void setAccountHolderName(string accountHolderName)
+        {
+            this.accountHolderName = accountHolderName;
+        }
+
+        public void setBalance(double balance)
+        {
+            this.balance = balance;
+        }
+
+
+
+        public int getAccountID()
+        {
+
+            return this.accountID;
+
+        }
+
+
+        public string getAccounHolderName()
+        {
+
+            return this.accountHolderName;
+
+        }
+
+
+        public double getBalance()
+        {
+
+            return this.balance;
+
+        }
+
+    }
+}
